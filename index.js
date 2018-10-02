0db99db1fe8fe14bc67ca510b98122d5e207151f
Jayon Kim <jpkim921@gmail.com>
2018-10-02 04:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function createFareMultiplier(int){
 
 // + `fareDoubler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` function accepts a fare as its lone argument and doubles it.
 
-const doubler = createFareMultiplier(2);
+const fareDoubler = createFareMultiplier(2);
 
 
 // + `fareTripler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareTripler()` function accepts a fare as its lone argument and triples it.
